959325e0ba9a5699b678fe339757387ce41d35aa
ondontoprojeto <54729178+ondontoprojeto@users.noreply.github.com>
2019-11-28 17:14:55 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -45,6 +45,15 @@
 			</div>
 				<?php }?>
 		</div>
+
+		<script>
+            setTimeout(function(){ 
+                var msg = document.getElementsByClassName("alert alert-danger mt-5 w-100");
+                while(msg.length > 0){
+                    msg[0].parentNode.removeChild(msg[0]);
+                }
+            }, 3000);
+        </script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
