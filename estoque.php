0d545e765dd96e77712991b1cc8cdc4c76afe4a2
ondontoprojeto <54729178+ondontoprojeto@users.noreply.github.com>
2019-12-06 16:02:09 -0300

    Add files via upload

--- estoque.php
+++ estoque.php
@@ -82,7 +82,7 @@
                     </div>
                 </div>
                 <div class = "overflow-auto ml-1 mr-1" style = "max-height: 550px">
-                    <table class="table w-100 mt-4">
+                    <table class="table w-100 mt-4 table-hover">
                         <thead class="thead-dark">
                             <tr>
                                 <th scope="col">Código</th>
@@ -143,7 +143,47 @@
                         </tbody>
                     </table>
                 </div>
-            </div>     
+            </div>
+
+            <?php 
+                if($quantidade < 10){
+            ?>    
+            <div class = " ml-5 w-25">              
+                <h3 class = "text-center">Favor, repor os seguintes produtos:</h3>
+                <table class="table w-100 mt-4 table-hover ml-2">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Nome</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+
+                    include_once 'conexao.php';
+
+                    $sql = "SELECT * FROM estoque WHERE quantidade <= 9";
+
+                    $busca = mysqli_query($con, $sql);
+
+                    while($array = mysqli_fetch_array($busca)){
+                    $nroProduto = $array['numeroproduto'];
+                    $nomeProduto = $array['nomeproduto'];
+
+                    //Ajuste da formatação da data DD/MM/AAAA
+                    $dtVenci = explode('-', $vencimento);
+                    $dtVencimento = $dtVenci[2] . "-" . $dtVenci[1]. "-" . $dtVenci[0];
+
+
+                    ?>
+                        <tr>
+                            <td><?php echo $nomeProduto?></td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+                
+            </div>
+            <?php }?>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
