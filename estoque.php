1344f8de14315825bcf0bf733c0835e0d4c91842
ondontoprojeto <54729178+ondontoprojeto@users.noreply.github.com>
2019-12-06 16:45:19 -0300

    Add files via upload

--- estoque.php
+++ estoque.php
@@ -102,9 +102,9 @@
 
                                 $sql = "SELECT * FROM estoque";
 
-                                $busca = mysqli_query($con, $sql);
+                                $buscar = mysqli_query($con, $sql);
 
-                                while($array = mysqli_fetch_array($busca)){
+                                while($array = mysqli_fetch_array($buscar)){
                                     $nroProduto = $array['numeroproduto'];
                                     $nomeProduto = $array['nomeproduto'];
                                     $categoria = $array['categoria'];
@@ -146,9 +146,10 @@
             </div>
 
             <?php 
-                if($quantidade < 10){
+                if(mysqli_num_rows($buscar) > 0){
+                    if($quantidade < 10){
             ?>    
-            <div class = " ml-5 w-25">              
+            <div class = "overflow-auto ml-5 w-25 mt-5" style = "max-height: 400px">              
                 <h3 class = "text-center">Favor, repor os seguintes produtos:</h3>
                 <table class="table w-100 mt-4 table-hover ml-2">
                     <thead class="thead-dark">
@@ -183,7 +184,8 @@
                 </table>
                 
             </div>
-            <?php }?>
+                <?php }; ?>
+            <?php } ?>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
